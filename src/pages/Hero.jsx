f9fd112c3d9b61086f42c62f55eaf63d466ac5c4
Yefry Sanchez <yefrymsp@gmail.com>
2023-01-17 15:15:36 -0500

    video bug on mobile fix

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <header id="home" className="relative h-screen w-full">
       <div className=" absolute w-full h-full bg-black opacity-50"></div>
-      <video src={videoHero} autoPlay loop muted className="h-full w-full object-cover"/>
+      <video src={videoHero} autoPlay loop muted playsInline className="h-full w-full object-cover"/>
       <div className="absolute select-none inset-0 flex justify-center items-center">
         <h1 className="text-[10vw] text-amber-400 font-MrDafoe lin drop-shadow-2xl leading-tight">Make the right <br /><span className="font-poppins font-bold text-white text-[15vw] ">move.</span></h1>
       </div>

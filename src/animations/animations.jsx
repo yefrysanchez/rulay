477baaf9f0b4af30cc86da43ee90e20b5fa34efc
Yefry Sanchez <yefrymsp@gmail.com>
2024-09-30 22:38:20 -0400

    new loader

--- src/animations/animations.jsx
+++ src/animations/animations.jsx
@@ -24,34 +24,18 @@ export const fade = {
     },
   };
 
-export const fadeUp = {
-  initial: {
-    y: 100,
-    opacity: 0,
-  },
-  enter: {
-    y: 0 ,
-    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
-    opacity: 1,
-  },
-  exit: {
-    y: 100 ,
-    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
-    opacity: 0,
-  },
-};
 
-// Define the variants for the parent and child animations
-export const container = {
-  initial: {},
-  enter: {
-    transition: {
-      staggerChildren: 0.2, // Adjust this for timing between each animation
+  export const scaleX = {
+    initial: {
+      scaleX: "0%",
+      scaleY: "0.1%",
     },
-  },
-  exit: {
-    transition: {
-      staggerChildren: 0.1, // Adjust this for timing between each animation
+    enter: {
+      scaleX: "100%",
+      transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
     },
-  },
-};
+    exit: {
+      scaleY: "100%",
+      transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+    },
+  };
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,5 @@
 import { AnimatePresence, motion, useAnimation } from "framer-motion";
-import { container, fadeUp, fade, slideRight } from "../animations/animations";
+import { fade, scaleX } from "../animations/animations";
 import { useEffect, useState } from "react";
 
 const Loader = () => {
@@ -17,38 +17,23 @@ const Loader = () => {
   return (
     <AnimatePresence>
       {isVisible && (
-        <motion.div variants={fade} initial="initial"
-        animate="enter"
-        exit="exit" className="h-screen fixed inset-0 bg-amber-500 z-[100] flex justify-center items-center">
-          <div className="h-fit max-w-sm lg:max-w-[50%] w-full px-4 flex flex-col justify-center">
-            <motion.div
-              variants={container}
-              initial="initial"
-              animate="enter"
-              exit="exit"
-              className="text-[8vw] lg:text-[4.2vw] text-center font-bold"
-            >
-              {words.map((w) => (
-                <motion.span
-                  variants={fadeUp}              
-                  className="mr-1 lg:mr-3"
-                  key={w}
-            
-                >
-                  {w}
-                </motion.span>
-              ))}
-            </motion.div>
-            <div className="relative w-full h-1 rounded-lg  overflow-hidden">
-              <motion.div
-                variants={slideRight}
-                initial="initial"
-                animate="enter"
-                exit="exit"
-                className="h-full w-full absolute bg-black rounded-lg"
-              ></motion.div>
-            </div>
-          </div>
+        <motion.div
+          variants={fade}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+          className="h-screen fixed inset-0 bg-amber-500 z-[100] flex justify-center items-center"
+        >
+          <h2 className="font-bold absolute top-1/2 -translate-y-1/2 text-center text-[7.4vw] text-white">
+            The Adventure Awaits
+          </h2>
+          <motion.div
+            variants={scaleX}
+            initial="initial"
+            animate="enter"
+            exit="exit"
+            className="h-full w-full bg-black"
+          ></motion.div>
         </motion.div>
       )}
     </AnimatePresence>
